c842c9a2720acd61a10cbbb1bcecbf4b5ddf706d
Ahmed Abououf <aabuauf@yahoo.com>
2019-10-20 12:27:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 
-function updateDriverWithKeyAndValue(driver, key, value){
-   driver[key]=value;
-   return driver
+function updateDriverWithKeyAndValue(drivers, key, value){
+   drivers[key]=value;
+   return drivers
 }
